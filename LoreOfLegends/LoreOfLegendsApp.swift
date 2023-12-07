72c63932fee1da735e672fe3f42dcab3b96c1e06
petermihok-shape <peter.mihok@shape.dk>
2023-12-07 21:09:48 +0100

    Initial Commit

---
+++ LoreOfLegends/LoreOfLegendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoreOfLegendsApp.swift
+//  LoreOfLegends
+//
+//  Created by Peter Mihók on 07/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoreOfLegendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
