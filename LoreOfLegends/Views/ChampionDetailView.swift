f7530ae0d192a2d13e52cb283d932e1eab43059a
mihokpeter <mihokpeter@pm.me>
2023-12-08 11:13:07 +0100

    Layout the ChampionDetailView

---
+++ LoreOfLegends/Views/ChampionDetailView.swift
@@ -0,0 +1,207 @@
+//
+//  ChampionDetailView.swift
+//  LoreOfLegends
+//
+//  Created by Peter Mihók on 07/12/2023.
+//
+
+import SwiftUI
+import CachedAsyncImage
+
+struct ChampionDetailView: View {
+    @EnvironmentObject private var viewModel: ChampionDetailViewModel
+    @State private var showFullLoreText: Bool = false
+    @State private var showFullSpellDescription: Bool = false
+
+    @State private var spells: [Champion.Spell] = []
+    @State private var skins: [Champion.Skin] = []
+    @State private var lore = ""
+    @State private var passive: Champion.Passive = Champion.Passive(name: "", description: "", image: Champion.Image(full: "", sprite: "", group: "", x: 0, y: 0, w: 0, h: 0))
+
+    let champion: Champion
+
+    init(champion: Champion) {
+        self.champion = champion
+    }
+
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 0) {
+                skinTabView
+                VStack(alignment: .leading) {
+                    championInfo
+                    championLore
+                    championSpells
+                }
+                .padding(.horizontal)
+                .padding(.bottom, 40)
+            }
+        }
+        .edgesIgnoringSafeArea(.all)
+        .scrollIndicators(.hidden)
+        .background(.darkBackground)
+        .task {
+            do {
+                let championData = try await viewModel.fetchChampionDetails(championID: champion.id)
+                if let championDetails = championData.data[champion.id] {
+                    skins = championDetails.skins ?? []
+                    lore = championDetails.lore ?? ""
+                    spells = championDetails.spells ?? []
+                    passive = championDetails.passive ?? .init(name: "", description: "", image: Champion.Image(full: "", sprite: "", group: "", x: 0, y: 0, w: 48, h: 48))
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+
+    private var skinTabView: some View {
+        TabView {
+            ForEach(skins) { skin in
+                CachedAsyncImage(
+                    url: URL(string: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/\(champion.id)_\(skin.num).jpg"),
+                    urlCache: URLCache.imageCache) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    } placeholder: {
+                        ProgressView()
+                    }
+            }
+        }
+        .tintPageViewControllers(pageIndicatorColor: .gold3, currentPageIndicatorColor: .gold3)
+        .tabViewStyle(PageTabViewStyle())
+        .frame(
+            width: UIScreen.main.bounds.width ,
+            height: UIScreen.main.bounds.height / 1.75
+        )
+    }
+
+    private var championInfo: some View {
+        return HStack(alignment: .lastTextBaseline, spacing: 10) {
+            Text(champion.name)
+                .font(Fonts.beaufortforLolBold.withSize(52))
+                .foregroundStyle(.gold2)
+
+            HStack(spacing: 0) {
+                Text("(\(champion.formattedTag))")
+                    .font(Fonts.beaufortforLolBold.withSize(15))
+                    .foregroundStyle(.gold2)
+            }
+            .padding(.bottom)
+        }
+    }
+
+    private var championLore: some View {
+        return Group {
+            Text("Lore:")
+                .font(Fonts.beaufortforLolBold.withSize(40))
+                .foregroundStyle(.gold2)
+
+            VStack {
+                Text(lore)
+                    .font(Fonts.beaufortforLolBold.withSize(18))
+                    .lineLimit(showFullLoreText ? nil : 5)
+                    .foregroundStyle(.gold1)
+
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        withAnimation {
+                            showFullLoreText.toggle()
+                        }
+                    }) {
+                        Text(showFullLoreText ? "Show Less" : "Show More")
+                            .font(Fonts.beaufortforLolBold.withSize(18))
+                            .foregroundStyle(.gold3)
+                    }
+                    .padding(.trailing)
+                }
+            }
+            .padding(.bottom, 20)
+        }
+    }
+
+    private var championSpells: some View {
+        return VStack(alignment: .leading) {
+            Text("Spells:")
+                .font(Fonts.beaufortforLolBold.withSize(40))
+                .foregroundStyle(.gold2)
+
+            HStack(spacing: 10) {
+                CachedAsyncImage(
+                    url: URL(string: "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/passive/\(passive.image.full)"),
+                    urlCache: URLCache.imageCache) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 60, height: 60)
+                            .clipShape(RoundedRectangle(cornerRadius: 30))
+                    } placeholder: {
+                        ProgressView()
+                    }
+
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(passive.name)
+                            .font(Fonts.beaufortforLolBold.withSize(20))
+                            .foregroundStyle(.gold3)
+                        Text("(Passive)")
+                            .font(Fonts.beaufortforLolBold.withSize(16))
+                            .foregroundStyle(.gold3)
+                    }
+                    Text(passive.removeHtmlTags(from: passive.description))
+                        .font(Fonts.beaufortforLolBold.withSize(12))
+                        .lineLimit(showFullSpellDescription ? nil : 2)
+                        .foregroundStyle(.gold1)
+                }
+            }
+            .onTapGesture {
+                withAnimation {
+                    showFullSpellDescription.toggle()
+                }
+            }
+            .padding()
+            .background(Color.grey1.opacity(0.2))
+            .clipShape(RoundedRectangle(cornerRadius: 15))
+
+            ForEach(spells) { spell in
+                HStack(spacing: 10) {
+                    CachedAsyncImage(
+                        url: URL(string: "https://ddragon.leagueoflegends.com/cdn/13.23.1/img/spell/\(spell.id).png"),
+                        urlCache: URLCache.imageCache) { image in
+                            image
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 60, height: 60)
+                                .clipShape(RoundedRectangle(cornerRadius: 30))
+                        } placeholder: {
+                            ProgressView()
+                        }
+
+                    VStack(alignment: .leading) {
+                        Text(spell.name)
+                            .font(Fonts.beaufortforLolBold.withSize(20))
+                            .foregroundStyle(.gold3)
+                        Text(spell.description)
+                            .font(Fonts.beaufortforLolBold.withSize(12))
+                            .lineLimit(showFullSpellDescription ? nil : 2)
+                            .foregroundStyle(.gold1)
+                    }
+                }
+                .onTapGesture {
+                    withAnimation {
+                        showFullSpellDescription.toggle()
+                    }
+                }
+                .padding()
+                .background(Color.grey1.opacity(0.2))
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+            }
+        }
+    }
+}
+
+#Preview {
+    ChampionDetailView(champion: Champion.exampleChampion)
+}
