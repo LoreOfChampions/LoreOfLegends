4dbdc3f69571baa7abae289a815e0a415e48d8a5
mihokpeter <mihokpeter@pm.me>
2023-12-08 11:10:23 +0100

    New ChampionDetailViewModel
    - create new view model to fetch all the necessary champion data with the latest version
    - this view model is also responsible for alphabetically sorting the champions array

---
+++ LoreOfLegends/ViewModels/ChampionDetailViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  ChampionDetailViewModel.swift
+//  LoreOfLegends
+//
+//  Created by Peter Mihók on 07/12/2023.
+//
+
+import Foundation
+
+class ChampionDetailViewModel: ObservableObject {
+    let version: String
+
+    init(version: String) {
+        self.version = version
+    }
+
+    //MARK: - Fetch all the extra data related to the specific champion from the latest version
+    func fetchChampionDetails(championID: String) async throws -> ChampionData {
+        let endpoint = Constants.buildURLEndpointString(version: self.version, championID: "/" + championID)
+
+        guard let url = URL(string: endpoint) else {
+            throw "Invalid URL"
+        }
+
+        let (data, response) = try await URLSession.shared.data(from: url)
+        try await Task.sleep(nanoseconds: NSEC_PER_SEC)
+
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw "Unable to complete the response"
+        }
+
+        let decoder = JSONDecoder()
+        let championDetails = try decoder.decode(ChampionData.self, from: data)
+
+        return championDetails
+    }
+}
