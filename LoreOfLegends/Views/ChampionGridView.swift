400f93c00259bd36a363e37a5addf3e32ea0452b
mihokpeter <mihokpeter@pm.me>
2023-12-08 11:12:52 +0100

    Layout the ChampionGridView

---
+++ LoreOfLegends/Views/ChampionGridView.swift
@@ -0,0 +1,30 @@
+//
+//  ChampionGridView.swift
+//  LoreOfLegends
+//
+//  Created by Peter Mihók on 07/12/2023.
+//
+
+import SwiftUI
+
+struct ChampionGridView: View {
+    @EnvironmentObject var viewModel: ChampionViewModel
+
+    private let columns: [GridItem] = [GridItem(.adaptive(minimum: 150))]
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            ForEach(viewModel.alphabeticallySortedChampions) { champion in
+                NavigationLink {
+                    ChampionDetailView(champion: champion)
+                } label: {
+                    ChampionGridCell(champion: champion)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ChampionGridView()
+}
