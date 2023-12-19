7f5298c6c0c54f80e342900c62ce4ca591a1416c
mihokpeter <mihokpeter@pm.me>
2023-12-19 15:11:23 +0100

    Add sampleParagraph constant
    - this servers to mock the champion's lore before its actually loaded

--- LoreOfLegends/Helpers/Constants.swift
+++ LoreOfLegends/Helpers/Constants.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct Constants {
     static let appTitle = "Lore of Legends"
+    static let sampleParagraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     static let darkBlueGradient: Gradient = Gradient(colors: [.blue6, .blue7])
     static let goldGradient: Gradient = Gradient(colors: [.gold5, .gold4])
     static let blueGradient: Gradient = Gradient(colors: [.blue4, .blue2])
