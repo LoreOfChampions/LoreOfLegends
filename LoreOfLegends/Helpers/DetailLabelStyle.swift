2958de3b735bb8965b503220865d5f88fbb3e248
petermihok-shape <peter.mihok@shape.dk>
2024-02-19 11:48:59 +0100

    New ViewModifier
    - create new view modifier for setting the text size and foreground color for majority of text elements in the app
    - to reduce the amount of modifiers in other views

---
+++ LoreOfLegends/Helpers/DetailLabelStyle.swift
@@ -0,0 +1,26 @@
+//
+//  DetailLabelStyle.swift
+//  LoreOfChampions
+//
+//  Created by Peter Mihók on 19/02/2024.
+//
+
+import SwiftUI
+
+struct DetailLabelStyle: ViewModifier {
+
+    let fontSize: CGFloat
+    let color: Color
+
+    func body(content: Content) -> some View {
+        content
+            .font(Fonts.beaufortforLolMedium.withSize(fontSize))
+            .foregroundStyle(color)
+    }
+}
+
+extension View {
+    func detailLabelStyle(fontSize: CGFloat, color: Color) -> some View {
+        modifier(DetailLabelStyle(fontSize: fontSize, color: color))
+    }
+}
