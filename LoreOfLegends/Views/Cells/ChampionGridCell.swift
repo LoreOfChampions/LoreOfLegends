df3a3f4cce71439b97b533214c4e3806d90c65f1
mihokpeter <mihokpeter@pm.me>
2024-04-17 21:25:43 +0200

    Use different CachedAsyncImage initializer

--- LoreOfLegends/Views/Cells/ChampionGridCell.swift
+++ LoreOfLegends/Views/Cells/ChampionGridCell.swift
@@ -10,29 +10,47 @@ import CachedAsyncImage
 import Shimmer
 
 struct ChampionGridCell: View {
-
     let champion: Champion
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            CachedAsyncImage(url: URL(string: "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/\(champion.id)_0.jpg"), urlCache: URLCache.imageCache) { image in
-                image
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-            } placeholder: {
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(.grey6)
-                    .frame(height: 326)
-                    .opacity(0.5)
-                    .overlay {
+            CachedAsyncImage(
+                url: URL(string: "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/\(champion.id)_0.jpg"),
+                urlCache: URLCache.imageCache) { phase in
+                    switch phase {
+                    case .empty:
                         RoundedRectangle(cornerRadius: 20)
-                            .fill(.grey4)
+                            .fill(.grey6)
                             .frame(height: 326)
-                            .shimmering()
+                            .opacity(0.5)
+                            .overlay {
+                                RoundedRectangle(cornerRadius: 20)
+                                    .fill(.grey4)
+                                    .frame(height: 326)
+                                    .shimmering()
+                            }
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                    case .failure:
+                        RoundedRectangle(cornerRadius: 20)
+                            .fill(.thinMaterial)
+                            .frame(height: 326)
+                            .overlay {
+                                VStack {
+                                    Image(systemName: "exclamationmark.triangle.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60, height: 60)
+                                    Text("Couldn't load the image!")
+                                }
+                            }
+                    @unknown default:
+                        fatalError()
                     }
-            }
-            .padding(.leading, -6)
+                }
 
             Text(champion.name)
                 .detailLabelStyle(fontSize: 26, color: .gold2)
