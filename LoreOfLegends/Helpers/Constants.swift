15a80bae0a5585dcdafe57caf4a084371341ad25
mihokpeter <mihokpeter@pm.me>
2024-04-17 21:08:37 +0200

    Remove optional from function's parameter
    - default it to an empty String instead of nil

--- LoreOfLegends/Helpers/Constants.swift
+++ LoreOfLegends/Helpers/Constants.swift
@@ -16,7 +16,7 @@ struct Constants {
     static let goldGradient: Gradient = Gradient(colors: [.gold5, .gold4])
     static let blueGradient: Gradient = Gradient(colors: [.blue4, .blue2])
 
-    static func buildURLEndpointString(version: String, championID: String? = nil) -> String {
-        return "https://ddragon.leagueoflegends.com/cdn/\(version)/data/en_US/champion\(championID ?? "").json"
+    static func buildURLEndpointString(version: String, championID: String = "") -> String {
+        return "https://ddragon.leagueoflegends.com/cdn/\(version)/data/en_US/champion\(championID).json"
     }
 }
