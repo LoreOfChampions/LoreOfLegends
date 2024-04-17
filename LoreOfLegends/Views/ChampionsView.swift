3bdb57ab7e7361425f58b9a33b653ef3ae66a50a
mihokpeter <mihokpeter@pm.me>
2024-04-17 21:14:22 +0200

    Refactor ChampionsView to work with DataService
    - remove unnecessary @State isLoading property
    - replace code within task modifier with loading champions and latest version from within view model
    - replace code within refreshable modifier with calling load from view model instead
    - switch on state inside NavigationStack
    - move all the UI code accordingly to the different switch state

--- LoreOfLegends/Views/ChampionsView.swift
+++ LoreOfLegends/Views/ChampionsView.swift
@@ -9,61 +9,67 @@ import SwiftUI
 
 struct ChampionsView: View {
     @EnvironmentObject private var viewModel: ChampionViewModel
-    @State private var isLoading: Bool = false
     @State private var shouldShowInfoView: Bool = false
 
     var body: some View {
         NavigationStack {
-            ScrollView {
-                ChampionGridView(isLoading: $isLoading)
-            }
-            .toolbar {
-                ToolbarItem(placement: .principal) {
-                    Text(Constants.appTitle)
-                        .detailLabelStyle(fontSize: 30, color: .gold3)
+            switch viewModel.state {
+            case .loading:
+                ProgressView()
+            case .loaded(let champions):
+                ScrollView {
+                    ChampionGridView(champions: champions)
                 }
-            }
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button {
-                        withAnimation(.easeIn) {
-                            shouldShowInfoView.toggle()
-                        }
-                    } label: {
-                        Image(systemName: "info.circle")
-                            .foregroundStyle(.gold3)
+                .toolbar {
+                    ToolbarItem(placement: .principal) {
+                        Text(Constants.appTitle)
+                            .detailLabelStyle(fontSize: 30, color: .gold3)
                     }
                 }
-            }
-            .searchable(text: $viewModel.searchingQuery, placement: .navigationBarDrawer(displayMode: .always))
-            .refreshable {
-                do {
-                    try await viewModel.getChampions()
-                } catch {
-                    print(LOLError.unableToDecodeData)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            withAnimation(.easeIn) {
+                                shouldShowInfoView.toggle()
+                            }
+                        } label: {
+                            Image(systemName: "info.circle")
+                                .foregroundStyle(.gold3)
+                        }
+                    }
                 }
-            }
-            .overlay {
-                if !viewModel.searchingQuery.isEmpty && viewModel.filteredChampions.isEmpty {
-                    ContentUnavailableView.search(text: viewModel.searchingQuery)
+                .searchable(text: $viewModel.searchingQuery, placement: .navigationBarDrawer(displayMode: .always))
+                .refreshable {
+                    await viewModel.load()
                 }
+                .overlay {
+                    if !viewModel.searchingQuery.isEmpty && viewModel.filteredChampions.isEmpty {
+                        ContentUnavailableView.search(text: viewModel.searchingQuery)
+                    }
+                }
+                .padding(.horizontal, 19)
+                .scrollIndicators(.hidden)
+                .background(.darkBackground)
+            case .error(let dataServiceError, let retry):
+                LOCErrorView(
+                    title: dataServiceError.errorTitle,
+                    message: dataServiceError.errorDescription ?? dataServiceError.localizedDescription,
+                    buttonTitle: dataServiceError.buttonTitle,
+                    buttonAction: {
+                        Task {
+                            await retry()
+                        }
+                    }
+                )
             }
-            .padding(.horizontal, 19)
-            .scrollIndicators(.hidden)
-            .background(.darkBackground)
         }
         .tint(.gold3)
         .overlay {
             InfoView(shouldShowInfoView: $shouldShowInfoView)
         }
         .task {
-            isLoading = true
-            do {
-                try await viewModel.getChampions()
-            } catch {
-                print(LOLError.unableToDecodeData)
-            }
-            isLoading = false
+            await viewModel.load()
+            await viewModel.loadLatestVersion()
         }
     }
 }
