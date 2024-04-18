7acd087b3999263290640d838255f0c9353c3fbb
mihokpeter <mihokpeter@pm.me>
2024-04-18 20:22:42 +0200

    Adjust ChampionGridCell
    - remove switching on phase and just use image and placeholder
    - add line limit to the champion name as it was breaking the UI when champion name was longer than that

--- LoreOfLegends/Views/Cells/ChampionGridCell.swift
+++ LoreOfLegends/Views/Cells/ChampionGridCell.swift
@@ -16,44 +16,28 @@ struct ChampionGridCell: View {
         VStack(alignment: .leading, spacing: 0) {
             CachedAsyncImage(
                 url: URL(string: "https://ddragon.leagueoflegends.com/cdn/img/champion/loading/\(champion.id)_0.jpg"),
-                urlCache: URLCache.imageCache) { phase in
-                    switch phase {
-                    case .empty:
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(.grey6)
-                            .frame(height: 326)
-                            .opacity(0.5)
-                            .overlay {
-                                RoundedRectangle(cornerRadius: 20)
-                                    .fill(.grey4)
-                                    .frame(height: 326)
-                                    .shimmering()
-                            }
-                    case .success(let image):
-                        image
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .clipShape(RoundedRectangle(cornerRadius: 20))
-                    case .failure:
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(.thinMaterial)
-                            .frame(height: 326)
-                            .overlay {
-                                VStack {
-                                    Image(systemName: "exclamationmark.triangle.fill")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 60, height: 60)
-                                    Text("Couldn't load the image!")
-                                }
-                            }
-                    @unknown default:
-                        fatalError()
-                    }
+                urlCache: URLCache.imageCache) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: 326)
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                } placeholder: {
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(.grey6)
+                        .frame(height: 326)
+                        .opacity(0.5)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20)
+                                .fill(.grey4)
+                                .frame(height: 326)
+                                .shimmering()
+                        }
                 }
 
             Text(champion.name)
                 .detailLabelStyle(fontSize: 26, color: .gold2)
+                .lineLimit(1)
 
             HStack(spacing: 0) {
                 Text(champion.formattedTag)
