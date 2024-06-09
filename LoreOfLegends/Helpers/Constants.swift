dd99e79db6d3261114e640d37f240b857411bd9f
mihokpeter <mihokpeter@pm.me>
2024-06-09 15:35:04 +0200

    [Settings-feature] Make Constants as Enum and not struct
    - store version URL to the property

--- LoreOfLegends/Helpers/Constants.swift
+++ LoreOfLegends/Helpers/Constants.swift
@@ -8,9 +8,10 @@
 import Foundation
 import SwiftUI
 
-struct Constants {
+enum Constants {
     static let appTitle = "LoreOfChampions"
     static let baseURL = "https://ddragon.leagueoflegends.com/cdn/"
+    static let versionsURL = "https://ddragon.leagueoflegends.com/api/versions.json"
     static let localesURL = "https://ddragon.leagueoflegends.com/cdn/languages.json"
     static let sampleParagraph = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     static let darkBlueGradient: Gradient = Gradient(colors: [.blue6, .blue7])
