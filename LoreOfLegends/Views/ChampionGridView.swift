af5ed3339313b00bea12b820ead88983775cab01
mihokpeter <mihokpeter@pm.me>
2024-12-07 18:49:47 +0100

    [FavoriteChampions] - Add moved parts from ChampionsView to this view

--- LoreOfLegends/Views/ChampionGridView.swift
+++ LoreOfLegends/Views/ChampionGridView.swift
@@ -10,18 +10,56 @@ import Shimmer
 
 struct ChampionGridView: View {
     @EnvironmentObject private var viewModel: ChampionViewModel
+    @State private var shouldPresentSheet: Bool = false
 
     private let columns: [GridItem] = [GridItem(.adaptive(minimum: 150, maximum: .infinity))]
     
     let champions: [Champion]
 
     var body: some View {
-        LazyVGrid(columns: columns, spacing: 20) {
-            if viewModel.selectedChampion != nil || !viewModel.searchingQuery.isEmpty {
-                SearchedChampionView()
-            } else {
-                AlphabeticallySortedChampionsView()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    if viewModel.selectedChampion != nil || !viewModel.searchingQuery.isEmpty {
+                        SearchedChampionView()
+                    } else {
+                        AlphabeticallySortedChampionsView()
+                    }
+                }
             }
+            .searchable(text: $viewModel.searchingQuery, placement: .navigationBarDrawer(displayMode: .always))
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text(Constants.appTitle)
+                        .detailLabelStyle(fontSize: 30, color: .gold3)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    SettingsButton(shouldPresentSheet: $shouldPresentSheet)
+                }
+            }
+        }
+        .overlay {
+            if !viewModel.searchingQuery.isEmpty && viewModel.filteredChampions.isEmpty {
+                ContentUnavailableView.search(text: viewModel.searchingQuery)
+            }
+        }
+        .fullScreenCover(isPresented: $shouldPresentSheet, content: {
+            SettingsView()
+        })
+    }
+}
+
+struct SettingsButton: View {
+    @Binding var shouldPresentSheet: Bool
+
+    var body: some View {
+        Button {
+            shouldPresentSheet = true
+        } label: {
+            Image(systemName: "gear")
+                .foregroundStyle(.gold3)
         }
     }
 }
